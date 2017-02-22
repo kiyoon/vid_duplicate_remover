6eafe8e2c23458a5ddf0c384135ec49a8fdb787d
Kiyoon Kim <yoonkr33@gmail.com>
2017-02-22 15:56:14 +0900

    least frame count feature added

--- dupremove.py
+++ dupremove.py
@@ -7,12 +7,14 @@ from os.path import join
 import numpy as np
 
 MSE_THRESHOLD = 0.5
+LEAST_FRAME = 18
 # threshold of mse value that will consider it as a duplicate.
 
 if len(sys.argv) < 3:
     print "Usage: %s [input_dir] [output_dir]"
     print "Author: Kiyoon Kim (yoonkr33@gmail.com)"
     print "Remove the duplicates of videos."
+    print "Least frame count is set to 18, because we need at least 8 optical flows per video."
     sys.exit()
 
 input_dir = sys.argv[1]
@@ -43,21 +45,33 @@ for root, dirs, files in os.walk(input_dir):
             print "(%d/%d) Generating to %s" % (i+j+1, total, out_file)
             if not os.path.exists(out_dir):
                 os.makedirs(out_dir)
-            cap = cv2.VideoCapture(in_file)
-            ret, frame = cap.read()
-            if not ret:
-                raise Exception('Unable to read the video.')
-            vid_writer = cv2.VideoWriter(out_file, fourcc, 25.0, (frame.shape[1],frame.shape[0]))
-            vid_writer.write(frame)
 
-            prev_frame = frame
-            ret, frame = cap.read()
-            while ret:
-                if ((frame - prev_frame)**2).mean() > MSE_THRESHOLD:
-                    vid_writer.write(frame)
+            threshold = MSE_THRESHOLD
+            while True:
+                fcount = 0
+                cap = cv2.VideoCapture(in_file)
+                ret, frame = cap.read()
+                if not ret:
+                    raise Exception('Unable to read the video.')
+                vid_writer = cv2.VideoWriter(out_file, fourcc, 25.0, (frame.shape[1],frame.shape[0]))
+                vid_writer.write(frame)
+                fcount += 1
+
                 prev_frame = frame
                 ret, frame = cap.read()
-            cap.release()
-            vid_writer.release()
+                while ret:
+                    if ((frame - prev_frame)**2).mean() > threshold:
+                        vid_writer.write(frame)
+                        fcount += 1
+                        prev_frame = frame
+                    ret, frame = cap.read()
+                cap.release()
+                vid_writer.release()
+                if fcount < LEAST_FRAME:
+                    # too few frames. do it again with lower threshold.
+                    os.remove(out_file)
+                    threshold *= 0.9
+                else:
+                    break
         i += len(videos)
 
